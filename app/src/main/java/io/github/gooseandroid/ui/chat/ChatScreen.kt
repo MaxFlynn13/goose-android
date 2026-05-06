04a8661fd2e5c12c8d4e4e41821c14e8a994e49c
Maxwell Flynn <jflynn@squareup.com>
2026-05-06 16:50:52 -0500

    Polish: remove unused sp import from ChatScreen

--- app/src/main/java/io/github/gooseandroid/ui/chat/ChatScreen.kt
+++ app/src/main/java/io/github/gooseandroid/ui/chat/ChatScreen.kt
@@ -41,7 +41,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import io.github.gooseandroid.data.SettingsKeys
 import io.github.gooseandroid.data.SettingsStore
 import kotlinx.coroutines.delay
